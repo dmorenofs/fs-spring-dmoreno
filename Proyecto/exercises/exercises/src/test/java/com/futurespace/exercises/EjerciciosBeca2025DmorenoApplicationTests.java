c53a61415d190e29fb5eb749c8572e9db5230035
dmorenofs <dmoreno@futurespace.es>
2025-02-24 10:13:25 +0100

    Add files via upload

---
+++ Proyecto/exercises/exercises/src/test/java/com/futurespace/exercises/EjerciciosBeca2025DmorenoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.futurespace.exercises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjerciciosBeca2025DmorenoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
